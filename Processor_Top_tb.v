16b557da6b853a6052467d72b7f38a3bb95fb631
ajaynair123 <ajaynair1998@gmail.com>
2021-03-28 18:13:56 +0530

    Testbench

--- Processor_Top_tb.v
+++ Processor_Top_tb.v
@@ -1,27 +1,5 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   19:45:43 02/11/2021
-// Design Name:   Processor_Top
-// Module Name:   C:/Xilinx/1_RTL/Processor/Processor_Top_tb.v
-// Project Name:  Processor
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: Processor_Top
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
-
 module Processor_Top_tb;
 
 // Inputs
